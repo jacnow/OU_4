616935c046068227accddb6cc14cf3b0bd634911
Jacek Nowakowski <jacnow@kis.p.lodz.pl>
2021-01-27 17:05:57 +0100

    Branch OU_01, metoda Policz - dodawanie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+int Policz (int a, int b) {
+    return a+b;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Branch OU_01, Policz 2+3= " << Policz(2,3) << std::endl;
     return 0;
 }
